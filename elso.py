a8c7b3fc65ecb1609ee2c4828b03f41eaae96e74
MaximMolnar1234 <maxim20060925@gmail.com>
2025-09-10 12:25:15 +0200

    szam lista generalas

---
+++ elso.py
@@ -0,0 +1,11 @@
+import random
+
+#Lista létrehozása
+szamok = []
+#Lista feltöltése 100 db random kétjegyű egész számmal
+for i in range(100):
+    szam = random.randint(10, 99)
+    szamok.append(szam)
+#Ellenőrizni
+print(szamok)
+print(len(szamok))         
