6acf5360a15f537d39894beaaa0d8b875610ae7a
MaximMolnar1234 <maxim20060925@gmail.com>
2025-09-17 11:22:43 +0200

    A tökéletes játék

--- elso.py
+++ elso.py
@@ -22,8 +22,8 @@ print(szamok)
 
 
 # Változók létrehozása statisztika készítéshez
-jatek_szam = 0
-nem_talaldDB = 0
+jatek_szam = 1
+nem_talaltdDB = 0
 
 # A kitalálandó szám kiválasztása a listából
 kitalalando_szam = szamok[random.randint(0, len(szamok))]
@@ -35,7 +35,8 @@ kitalalando_szam = 12
 jatszol = True
 
 while(jatszol):
-    jatek_szam += 1    
+    
+        
     tipp_sz = input("Tipped? (egész szám): ").strip()
     if(tipp_sz.isdecimal()):
         tipp = int(tipp_sz)
@@ -44,27 +45,38 @@ while(jatszol):
         continue
 
     while(tipp != kitalalando_szam):
+        nem_talaltdDB += 1
         
-        if (tipp < kitalalando_szam):
+        if (tipp == 123):
+            pass
+        elif (tipp < kitalalando_szam):
             print("A kitalálandó szám nagyobb!")
-        else:
+            nem_talaltdDB += 1
+        elif(tipp > kitalalando_szam):
             print("A kitalálandó szám kisebb!")
-                
-        
+            nem_talaltdDB += 1
+               
         tipp_sz = input("Tipped? (egész szám)\n Kilépés \'X\' karakterrel: ").strip()
+        
         if(tipp_sz.isdecimal()):
             tipp = int(tipp_sz)
         elif tipp_sz == 'X' or tipp_sz == 'x':
+            print(f"\nJátékok száma: {jatek_szam} Próbálkozások száma: {nem_talaltdDB}")
             exit()    
         else:
             print("Egész számmal játsz!")
+            tipp = 123
             continue
 
     print("Kitaláltad a kitalálandó számot!")
 
     folytatas = input("Akarsz-e még játszani? [I/N]")
+    
     if(folytatas == "N"):
         jatszol = False
+        print(f"\nJátékok száma: {jatek_szam} Próbálkozások száma: {nem_talaltdDB}")
+    elif(folytatas == "I"): 
+        jatek_szam+=1    
     
           
             
